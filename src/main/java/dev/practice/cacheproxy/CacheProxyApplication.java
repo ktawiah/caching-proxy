38f30692e800d3d5dd9ab599a3fc971c223119a0
Kelvin <kelvintawiah224@gmail.com>
2025-02-23 20:59:00 -0600

    setup project

---
+++ src/main/java/dev/practice/cacheproxy/CacheProxyApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.cacheproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/practice/cacheproxy/CacheProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.cacheproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
