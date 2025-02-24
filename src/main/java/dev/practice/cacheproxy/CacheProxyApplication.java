aed34a29ff0b942005a3f088c132d865b373c7c7
Kelvin <kelvintawiah224@gmail.com>
2025-02-24 02:41:52 -0600

    fix: update app properties

--- src/main/java/dev/practice/cacheproxy/CacheProxyApplication.java
+++ src/main/java/dev/practice/cacheproxy/CacheProxyApplication.java
@@ -2,7 +2,9 @@ package dev.practice.cacheproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
+@CommandScan
 @SpringBootApplication
 public class CacheProxyApplication {
 
